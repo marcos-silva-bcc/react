750d69dd50db6aad4a00d8b70a392de0251d3788
Marcos <mfsj25@inf.ufpr.br>
2025-10-30 22:12:01 -0300

    Aula 9 - Seção 6

--- src/Components/Forms.tsx
+++ src/Components/Forms.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { FormEvent, useRef } from 'react'
 
 const Forms = () => {
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (nameRef.current !== null)
+      person.name = nameRef.current.value
+    if (ageRef.current !== null)
+      person.age = parseInt(ageRef.current.value)
+    console.log(person)
+  };
+
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = {name: '', age: 0}
+
   return (
-    <form>
+
+    <form onSubmit={handleSubmit}>
         <div className="mb-3">
-            <label htmlFor="Formulário" className="form-label">Nome</label><input id="Name" type="text" className="form-control"/>
+            <label htmlFor="Formulário" className="form-label">Nome</label>
+            <input ref={nameRef} id="Name" type="text" className="form-control"/>
         </div>
-        <div className="mb-3"><label htmlFor="Age" className="form-label">Idade</label><input type="number" id = "Age" className="form-control" /></div>
+        <div className="mb-3">
+          <label htmlFor="Age" className="form-label">Age</label>
+          <input ref={ageRef} type="number" className="form-control" />
+        </div>
+        
+        <button className="btn btn-primary" type='submit'>Submit</button>
 
     </form>
   )
